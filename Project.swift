781bdaef39bbf045040f9ef446658781e78499b9
Do-hyun-Kim <dohyun8032@gmail.com>
2022-06-02 16:12:35 +0900

    refactor: Tuist 프로젝트 설정코드 변경
     - 프로젝트 설정 커스텀을 위해 Project.swift 기본 설정코드 제거 및 변경
     - 외부 라이브러리(ReactorKit, SnapKit) 설정을 위한 Dependencies.swift 파일 및 설정코드 추가

--- Project.swift
+++ Project.swift
@@ -1,25 +1,88 @@
 import ProjectDescription
-import ProjectDescriptionHelpers
 
-/*
-                +-------------+
-                |             |
-                |     App     | Contains Sponge App target and Sponge unit-test target
-                |             |
-         +------+-------------+-------+
-         |         depends on         |
-         |                            |
- +----v-----+                   +-----v-----+
- |          |                   |           |
- |   Kit    |                   |     UI    |   Two independent frameworks to share code and start modularising your app
- |          |                   |           |
- +----------+                   +-----------+
+let projectName: String = "connect"
+let organizationName: String = "sideproj"
+let bundleName: String = "com.sideproj"
+let bundleID: String = "com.sideproj.connect"
 
- */
+let baseSetting: [String: SettingValue] = [
+  "DEBUG_INFORMATION_FORMAT": "dwarf-with-dsym",
+  "OTHER_LDFLAGS": "$(inherited) -Xlinker -interposable"
+]
 
-// MARK: - Project
+/// info.plist 내용 지정.
+let infoPlist: [String: InfoPlist.Value] = [
+  "CFBundleName": "\(projectName)",
+  "CFBundleDisplayName": "\(projectName)",
+  "CFBundleIdentifier": "\(bundleID)",
+  "CFBundleShortVersionString": "1.0",
+  "CFBundleVersion": "0",
+  "CFBuildVersion": "0",
+  "UILaunchStoryboardName": "Launch Screen",
+  "UISupportedInterfaceOrientations": ["UIInterfaceOrientationPortrait"],
+  "UIUserInterfaceStyle": "Light",
+  "UIApplicationSceneManifest": [
+    "UIApplicationSupportsMultipleScenes": false,
+    "UISceneConfigurations": [
+      "UIWindowSceneSessionRoleApplication": [
+        [
+          "UISceneConfigurationName": "Default Configuration",
+          "UISceneDelegateClassName": "$(PRODUCT_MODULE_NAME).SceneDelegate"
+        ]
+      ]
+    ]
+  ]
+]
 
-// Creates our project using a helper function defined in ProjectDescriptionHelpers
-let project = Project.app(name: "Sponge",
-                          platform: .iOS,
-                          additionalTargets: ["SpongeKit", "SpongeUI"])
+let settings: Settings = .settings(
+  base: baseSetting,
+  configurations: [
+    .release(name: .release)
+  ],
+  defaultSettings: .recommended
+)
+
+let targets = [
+  Target(
+    name: projectName,
+    platform: .iOS,
+    product: .app,
+    bundleId: "\(bundleID)",
+    deploymentTarget: .iOS(targetVersion: "15.0", devices: [.iphone]),
+    infoPlist: .extendingDefault(with: infoPlist),
+    sources: "Sources/**",
+    resources: "Resources/**",
+    dependencies: [
+      /// Dependencies.swift에 정의된 외부 라이브러리 설정
+      .external(name: "ReactorKit"),
+      .external(name: "SnapKit")
+    ],
+    settings: settings
+  ),
+  Target(
+    name: "\(projectName)Tests",
+    platform: .iOS,
+    product: .unitTests,
+    bundleId: "\(bundleName).\(projectName)Tests",
+    sources: "\(projectName)Tests/**",
+    dependencies: [
+      .target(name: projectName)
+    ]
+        ),
+  Target(
+    name: "\(projectName)UITests",
+    platform: .iOS,
+    product: .uiTests,
+    bundleId: "\(bundleName).\(projectName)UITests",
+    sources: "\(projectName)UITests/**",
+    dependencies: [
+      .target(name: projectName)
+    ]
+  )
+]
+
+let project = Project(
+  name: projectName,
+  organizationName: organizationName,
+  targets: targets
+)
