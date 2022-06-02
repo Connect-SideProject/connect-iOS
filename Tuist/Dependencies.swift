489d897f2610e69aceae3602cc0df7156b3c2427
butter-free <butterfree@kakao.com>
2022-06-02 16:12:35 +0900

    refactor: Tuist 프로젝트 설정코드 변경
     - 프로젝트 설정 커스텀을 위해 Project.swift 기본 설정코드 제거 및 변경
     - 외부 라이브러리(ReactorKit, SnapKit) 설정을 위한 Dependencies.swift 파일 및 설정코드 추가

---
+++ Tuist/Dependencies.swift
@@ -0,0 +1,23 @@
+//
+//  Dependencies.swift
+//  ProjectDescriptionHelpers
+//
+//  Created by sean on 2022/06/02.
+//
+
+import ProjectDescription
+
+/// 외부 dependencies 업데이트 및 반영을 위해서 'tuist fetch' 명령어 실행.
+let dependencies = Dependencies(
+  swiftPackageManager: [
+    .remote(
+      url: "https://github.com/ReactorKit/ReactorKit.git",
+      requirement: .upToNextMajor(from: "3.0.0")
+    ),
+    .remote(
+      url: "https://github.com/SnapKit/SnapKit.git",
+      requirement: .upToNextMajor(from: "5.0.1")
+    )
+  ],
+  platforms: [.iOS]
+)
