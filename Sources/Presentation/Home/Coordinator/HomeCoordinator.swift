a5f79567eb436a3a345e38a0887b7d582b02e53a
Do-hyun-Kim <dohyun8032@gmail.com>
2022-06-12 20:06:25 +0900

    feat: Home Coordinator Folder 생성 및 DI 구조 변경
    DIP 원칙으로 클래스 의존성 제거 및 프로토콜을 통하여 MainTabbar ViewControllers 추가

---
+++ Sources/Presentation/Home/Coordinator/HomeCoordinator.swift
@@ -0,0 +1,28 @@
+//
+//  HomeCoordinator.swift
+//  connectUITests
+//
+//  Created by Kim dohyun on 2022/06/12.
+//  Copyright © 2022 sideproj. All rights reserved.
+//
+
+import UIKit
+
+
+/// 홈 코디네이터
+class HomeCoordinator: BaseCoordinator {
+    var presenter: UINavigationController
+    
+    var childrenCoordinator: [BaseCoordinator]
+    
+    init(presenter: UINavigationController) {
+        self.presenter = presenter
+        self.childrenCoordinator = []
+    }
+    
+    func moveToListDetail() {
+        let listVC = PostListController()
+        presenter.pushViewController(listVC, animated: true)
+    }
+    
+}
