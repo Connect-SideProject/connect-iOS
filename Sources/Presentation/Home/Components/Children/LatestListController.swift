1b028ee3c0e45ab7f5f379a57ed7ab6bde86a229
Do-hyun-Kim <dohyun8032@gmail.com>
2022-06-18 18:25:52 +0900

    feat: PostListController Container Method 추가
    PostListController Container Method extension 통해 추가 or Children 폴더 생성

---
+++ Sources/Presentation/Home/Components/Children/LatestListController.swift
@@ -0,0 +1,23 @@
+//
+//  LatestListController.swift
+//  connectUITests
+//
+//  Created by Kim dohyun on 2022/06/18.
+//  Copyright © 2022 sideproj. All rights reserved.
+//
+
+import UIKit
+
+
+final class LatestListController: UIViewController {
+    
+    deinit {
+        print(#function)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+}
