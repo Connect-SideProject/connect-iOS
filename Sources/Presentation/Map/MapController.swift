05d575085b4710254784fda354d20e046033a800
dlrjswns <leekyunjun@gmail.com>
2022-06-29 22:45:25 +0900

    feat: 주소에 따른 위치정보를 위한 MapAddress Entity 추가

--- Sources/Presentation/Map/MapController.swift
+++ Sources/Presentation/Map/MapController.swift
@@ -15,9 +15,15 @@ class MapController: UIViewController {
     
     private lazy var floatingPanelVC = FloatingPanelController()
     
-    private let mapSearchView: MapSearchView = {
-       let mapSearchView = MapSearchView()
-        return mapSearchView
+//    private let mapSearchView: MapSearchView = {
+//       let mapSearchView = MapSearchView()
+//        return mapSearchView
+//    }()
+    
+    private let mapSearchBar: UISearchBar = {
+       let searchBar = UISearchBar()
+        searchBar.placeholder = "지역을 검색해주세요"
+        return searchBar
     }()
     
     private let mapView: MTMapView = {
@@ -35,22 +41,28 @@ class MapController: UIViewController {
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         
-        mapSearchView.snp.makeConstraints { make in
-            make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
-            make.left.right.equalToSuperview()
-            make.height.equalTo(50)
-        }
+//        mapSearchView.snp.makeConstraints { make in
+//            make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+//            make.left.right.equalToSuperview()
+//            make.height.equalTo(50)
+//        }
         
         mapView.snp.makeConstraints { make in
-            make.top.equalTo(mapSearchView.snp.bottom)
-            make.left.right.bottom.equalToSuperview()
+            make.edges.equalToSuperview()
         }
+        
+//        mapSearchBar.snp.makeConstraints { make in
+//            make.centerY.equalToSuperview()
+//            make.top.left.equalToSuperview().offset(10)
+//            make.right.equalToSuperview().offset(-10)
+//            make.height.equalTo(50)
+//        }
     }
     
     //MARK: -Configure
     private func configureUI() {
-        view.addSubview(mapSearchView)
         view.addSubview(mapView)
+//        view.addSubview(mapSearchBar)
     }
     
     private func setFloatingPanel(contentViewController: UIViewController, _ floatingPanelVC: FloatingPanelController) {
@@ -65,52 +77,52 @@ class MapController: UIViewController {
 }
 
 //MARK: -Custom View
-extension MapController {
-    class MapSearchView: UIView {
-        
-        private let searchBar: UISearchBar = {
-           let searchBar = UISearchBar()
-            searchBar.placeholder = "프로젝트와 스터디를 검색해보세요."
-            return searchBar
-        }()
-        
-        private let filterButton: UIButton = {
-           let button = UIButton()
-            button.setImage(UIImage(systemName: "slider.horizontal.3"), for: .normal)
-            return button
-        }()
-        
-        override init(frame: CGRect) {
-            super.init(frame: frame)
-            configureUI()
-        }
-        
-        required init?(coder: NSCoder) {
-            fatalError("init(coder:) has not been implemented")
-        }
-        
-        override func layoutSubviews() {
-            super.layoutSubviews()
-            searchBar.snp.makeConstraints { make in
-                make.left.equalToSuperview().offset(10)
-                make.right.equalTo(filterButton.snp.left).offset(-10)
-                make.height.equalToSuperview()
-                make.centerY.equalToSuperview()
-            }
-            
-            filterButton.snp.makeConstraints { make in
-                make.centerY.equalToSuperview()
-                make.right.equalToSuperview().offset(-10)
-            }
-        }
-        
-        private func configureUI() {
-            self.backgroundColor = .systemBackground
-            self.addSubview(searchBar)
-            self.addSubview(filterButton)
-        }
-    }
-}
+//extension MapController {
+//    class MapSearchView: UIView {
+//
+//        private let searchBar: UISearchBar = {
+//           let searchBar = UISearchBar()
+//            searchBar.placeholder = "프로젝트와 스터디를 검색해보세요."
+//            return searchBar
+//        }()
+//
+//        private let filterButton: UIButton = {
+//           let button = UIButton()
+//            button.setImage(UIImage(systemName: "slider.horizontal.3"), for: .normal)
+//            return button
+//        }()
+//
+//        override init(frame: CGRect) {
+//            super.init(frame: frame)
+//            configureUI()
+//        }
+//
+//        required init?(coder: NSCoder) {
+//            fatalError("init(coder:) has not been implemented")
+//        }
+//
+//        override func layoutSubviews() {
+//            super.layoutSubviews()
+//            searchBar.snp.makeConstraints { make in
+//                make.left.equalToSuperview().offset(10)
+//                make.right.equalTo(filterButton.snp.left).offset(-10)
+//                make.height.equalToSuperview()
+//                make.centerY.equalToSuperview()
+//            }
+//
+//            filterButton.snp.makeConstraints { make in
+//                make.centerY.equalToSuperview()
+//                make.right.equalToSuperview().offset(-10)
+//            }
+//        }
+//
+//        private func configureUI() {
+//            self.backgroundColor = .systemBackground
+//            self.addSubview(searchBar)
+//            self.addSubview(filterButton)
+//        }
+//    }
+//}
 
 //MARK: -FloatingPanel Setting
 extension MapController: FloatingPanelControllerDelegate {
