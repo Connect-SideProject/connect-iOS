00af83ea20d4c3d9afe6f190f497666ff2ed4df7
Kim dohyun <>
2022-07-23 20:28:15 +0900

    feat: HomeSearchHeaderView File 생성 or  HomeControllerView UI 수정
    - UIView Extension 파일은 추후 Shadow, Layer에 대한 프로퍼티, 메서드 추가 하겠습니다

---
+++ Sources/Presentation/Home/Components/Header/HomeSearchHeaderView.swift
@@ -0,0 +1,64 @@
+//
+//  HomeSearchHeaderView.swift
+//  connect
+//
+//  Created by Kim dohyun on 2022/07/23.
+//  Copyright © 2022 sideproj. All rights reserved.
+//
+
+import UIKit
+
+
+final class HomeSearchHeaderView: UICollectionReusableView {
+    
+    
+    //MARK: Property
+    
+    private let mainTitleLabel: UILabel = {
+        $0.text = "어떤 프로젝트를 찾으시나요?"
+        $0.font = .systemFont(ofSize: 16, weight: .medium)
+        $0.textColor = .black
+        $0.textAlignment = .left
+        
+        return $0
+    }(UILabel())
+    
+    private let searchView: HomeSearchView = {
+        $0.layer.borderColor = .init(red: 187/255, green: 237/255, blue: 80/255, alpha: 1.0)
+        $0.layer.borderWidth = 2
+        return $0
+    }(HomeSearchView(frame: .zero))
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        debugPrint(#function)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    
+    private func configure() {
+        
+        _ = [mainTitleLabel,searchView].map { addSubview($0) }
+        
+        mainTitleLabel.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(30)
+            $0.left.equalToSuperview().offset(19)
+            $0.width.lessThanOrEqualTo(200)
+        }
+        
+        
+        searchView.snp.makeConstraints {
+            $0.top.equalTo(mainTitleLabel.snp.bottom).offset(11)
+            $0.left.equalToSuperview().offset(20)
+            $0.right.equalToSuperview().offset(-20)
+            $0.height.equalTo(42)
+            
+        }
+        
+    }
+    
+}
