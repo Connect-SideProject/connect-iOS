0e6ff7f66051822d5b065a2a0832f41fc5a38d28
Do-hyun-Kim <dohyun8032@gmail.com>
2022-07-23 13:43:29 +0900

    docs, feat: Network  연관 코드 및 디렉토리 추가
     - 기본 network 응답 포맷 처리로 Base Entity 추가
     - network 요청시 경로, 메서드, 파라미터 처리 등을 위해 EndPoint 추가
     - 테스트코드 활용을 위해 ApiManagerStub, SampleData 추가
     - 편의성을 위한 userDefaults 및 Encodable helper 메서드 추가

---
+++ Sources/Data/Network/EndPoint/EndPoint.swift
@@ -0,0 +1,86 @@
+//
+//  EndPoint.swift
+//  connect
+//
+//  Created by sean on 2022/07/23.
+//  Copyright © 2022 sideproj. All rights reserved.
+//
+
+import Foundation
+
+enum PathType {
+  case userProfile
+  case updateProfile(Profile)
+}
+
+enum HTTPMethod {
+  case get, post, put, delete
+  
+  var string: String {
+    switch self {
+    case .get:
+      return "GET"
+    case .post:
+      return "POST"
+    case .put:
+      return "PUT"
+    case .delete:
+      return "DELETE"
+    }
+  }
+}
+
+struct EndPoint {
+  private var baseURL: URL {
+    var components = URLComponents()
+    components.scheme = "https"
+    components.host = ""
+    return components.url!
+  }
+  
+  let path: PathType
+  
+  init(path: PathType) {
+    self.path = path
+  }
+}
+
+extension EndPoint {
+  
+  var header: [String: String]? {
+    return ["Authorization": UserManager.shared.accessToken]
+  }
+  
+  var url: URL? {
+    
+    var components = URLComponents()
+    components.scheme = baseURL.scheme
+    components.host = baseURL.host
+    
+    var queryItems: [URLQueryItem] = []
+    
+    switch path {
+    case .userProfile:
+      components.path = ""
+    case .updateProfile:
+      components.path = ""
+    }
+    
+    components.queryItems = queryItems
+    
+    return components.url
+  }
+  
+  var parameter: [String: Any] {
+    switch path {
+    case let .updateProfile(profile):
+      return profile.asDictionary()!
+    default:
+      return [:]
+    }
+  }
+  
+  var method: HTTPMethod {
+    return .get
+  }
+}
