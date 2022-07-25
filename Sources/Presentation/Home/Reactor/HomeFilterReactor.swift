518d5be1bcfdc41b607dd415d4d7d6ed51da6419
Do-hyun-Kim <dohyun8032@gmail.com>
2022-07-25 22:51:22 +0900

    feat: HomeController refactor RxDataSource  or HomeFilterReactor inject

---
+++ Sources/Presentation/Home/Reactor/HomeFilterReactor.swift
@@ -0,0 +1,36 @@
+//
+//  HomeFilterReactor.swift
+//  connect
+//
+//  Created by Kim dohyun on 2022/07/25.
+//  Copyright © 2022 sideproj. All rights reserved.
+//
+
+import ReactorKit
+
+
+final class HomeFilterReactor: Reactor {
+    
+    let initialState: State
+    
+    enum Action {
+        case didSelectSearchView
+    }
+    
+    enum Mutation {
+        case willAppearSearchView(Void)
+    }
+    
+    struct State {
+        var isLoading: Bool
+    }
+    
+    
+    init() {
+        defer { _ = self.state }
+        self.initialState = State(isLoading: false)
+    }
+    
+    
+    
+}
