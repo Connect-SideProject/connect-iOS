31fad7bc1c0cb57a98b819af5e93230f76dd6744
Do-hyun-Kim <dohyun8032@gmail.com>
2022-08-07 00:30:51 +0900

    docs: Extension 추가
    - UIViewController의 라이프사이클 이벤트를 Rx로 감지하기 위해서 Reactive Extension 추가
    - 제일 상위의 ViewController를 간편하게 가져오기 위해 UIApplication Extension 형태로 추가

--- Sources/Extensions/UIApplication+Extension.swift
+++ Sources/Extensions/UIApplication+Extension.swift
@@ -17,4 +17,21 @@ extension UIApplication {
       .flatMap { $0.windows }
       .first { $0.isKeyWindow }
   }
+  
+  // https://stackoverflow.com/questions/26667009/get-top-most-uiviewcontroller
+  class func getTopViewController(
+    base: UIViewController? = UIApplication.keyWindow?.rootViewController
+  ) -> UIViewController? {
+
+      if let nav = base as? UINavigationController {
+          return getTopViewController(base: nav.visibleViewController)
+
+      } else if let tab = base as? UITabBarController, let selected = tab.selectedViewController {
+          return getTopViewController(base: selected)
+
+      } else if let presented = base?.presentedViewController {
+          return getTopViewController(base: presented)
+      }
+      return base
+  }
 }
