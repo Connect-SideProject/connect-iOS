b329b25210589e065e9736f8213470055dee1570
Do-hyun-Kim <dohyun8032@gmail.com>
2022-09-12 16:17:56 +0900

    feat: 공통응답 형태 변경으로 Base Entity 내용 및 Network처리 변경

--- Domain/CODomain/Sources/Entity/Base.swift
+++ Domain/CODomain/Sources/Entity/Base.swift
@@ -8,36 +8,56 @@
 
 import Foundation
 
+public enum ResponseStatus: Decodable {
+  case success, failed
+  
+  public init(from decoder: Decoder) throws {
+    let container = try decoder.singleValueContainer().decode(String.self)
+    
+    switch container {
+    case "SUCCESS":
+      self = .success
+    default:
+      self = .failed
+    }
+  }
+}
+
 /// 공통 응답 Entity
 public struct Base<T>: Decodable where T: Decodable {
   
-  /// 응답 상태 코드.
-  public let statusCode: Int
-  /// 에러 메시지 내용.
+  /// 응답 상태 메시지.
+  public let status: ResponseStatus
+  /// 응답 메시지 내용.
   public let message: String
   /// 응답 결과.
   public let data: T?
+  /// 에러 코드 (디버깅).
+  public let errorCode: Int?
 
   public init(from decoder: Decoder) throws {
-    let container =   try decoder.container(keyedBy: CodingKeys.self)
+    let container =  try decoder.container(keyedBy: CodingKeys.self)
 
-    self.statusCode = try container.decodeIfPresent(Int.self, forKey: .statusCode) ?? -1
-    self.message =    try container.decodeIfPresent(String.self, forKey: .message) ?? ""
-    self.data =       try container.decodeIfPresent(T.self, forKey: .data)
+    self.status =    try container.decodeIfPresent(ResponseStatus.self, forKey: .status) ?? .failed
+    self.message =   try container.decodeIfPresent(String.self, forKey: .message) ?? ""
+    self.data =      try container.decodeIfPresent(T.self, forKey: .data)
+    self.errorCode = try container.decodeIfPresent(Int.self, forKey: .errorCode)
   }
 
   enum CodingKeys: String, CodingKey {
-    case statusCode = "code"
-    case message = "msg"
+    case status = "result"
+    case message
     case data
+    case errorCode = "error_code"
   }
 }
 
 public extension Base {
-  init(statusCode: Int, message: String, data: T?) {
-    self.statusCode = statusCode
+  init(status: ResponseStatus, message: String, data: T?) {
+    self.status = status
     self.message = message
     self.data = data
+    self.errorCode = nil
   }
 }
 
