889f279e4e0e3eb82dbe2e2eed286d7e545dce3f
Taeyoung Son <taeyoungson10@gmail.com>
2022-11-03 00:26:16 +0900

    feat: Container 및 TableView 배치

--- App/Sources/Presentation/Chat/ChatListController.swift
+++ App/Sources/Presentation/Chat/ChatListController.swift
@@ -11,16 +11,33 @@ import UIKit
 import RxSwift
 import RxDataSources
 import ReactorKit
+import FlexLayout
+import COExtensions
 
 final class ChatListController: ChatBaseController<ChatListController.Reactor> {
+    
     private let tableView = UITableView()
     
     override func setupContainer() {
+        super.setupContainer()
         
+        self.rootContainer.flex.define { [weak self] in
+            guard let self = self else { return }
+            $0.addItem(self.tableView)
+                .grow(1)
+        }
     }
     
     override func setAttrs() {
-        
+        self.navigationController?.setNavigationBarHidden(true, animated: false)
+        self.rootContainer.backgroundColor = .yellow
+        self.tableView.backgroundColor = .blue
+    }
+    
+    override func layout() {
+        self.rootContainer.pin.all()
+        let safe = UIApplication.keyWindow?.safeAreaInsets
+        self.rootContainer.flex.margin(safe ?? self.view.safeAreaInsets).layout()
     }
     
     override func bind(reactor: Reactor) {
