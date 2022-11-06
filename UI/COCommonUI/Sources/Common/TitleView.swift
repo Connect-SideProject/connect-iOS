44946f42f6a578673b1a8123171e2348033b25f6
Taeyoung Son <taeyoungson10@gmail.com>
2022-11-06 15:46:18 +0900

    feat: TitleView 레이아웃 추가

--- UI/COCommonUI/Sources/Common/TitleView.swift
+++ UI/COCommonUI/Sources/Common/TitleView.swift
@@ -9,6 +9,36 @@ import UIKit
 
 import FlexLayout
 
-class TitleView: UIView {
+public class TitleView: FlexLayoutView {
+    private let leftBtn = UIButton()
+    private let titleLabel = UILabel()
+    private let rightBtns = [UIButton(), UIButton()]
     
+    public override func setupContainer() {
+        super.setupContainer()
+        
+        self.rootContainer.flex
+            .direction(.row)
+            .alignItems(.center)
+            .define { [weak self] flex in
+                guard let self = self else { return }
+                
+                flex.addItem(self.leftBtn)
+                    .marginLeft(28)
+                    .width(17).height(20)
+                
+                flex.addItem(self.titleLabel)
+                    .marginLeft(12)
+                    .maxWidth(60%)
+                
+                flex.addItem()
+                    .grow(1)
+                
+                self.rightBtns.enumerated().forEach { offset, btn in
+                    flex.addItem(btn)
+                        .width(24).height(24)
+                        .marginEnd(20)
+                }
+            }
+    }
 }
