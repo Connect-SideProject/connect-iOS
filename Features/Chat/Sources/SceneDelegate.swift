6b9fffb01a6df981d6c268f651a35f2d3645cf51
Taeyoung Son <taeyoungson10@gmail.com>
2022-11-10 23:23:41 +0900

    feat: ChatDemoApp 의 Entry point 지정 - App/SceneDelegate 추가

---
+++ Features/Chat/Sources/SceneDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  SceneDelegate.swift
+//  ChatDemoApp
+//
+//  Created by Taeyoung Son on 2022/11/10.
+//
+
+import UIKit
+
+import CODomain
+import COManager
+import CONetwork
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+    var window: UIWindow?
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let scene = (scene as? UIWindowScene) else { return }
+        self.window = .init(windowScene: scene)
+        
+        let chatListC = ChatListDIContainer().makeVC()
+        
+        let navC = UINavigationController(rootViewController: chatListC)
+        self.window?.rootViewController = navC
+        self.window?.makeKeyAndVisible()
+    }
+}
