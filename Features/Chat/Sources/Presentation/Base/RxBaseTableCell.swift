87e6ca3a7c504a9b8304991c20ba1228551fd099
Taeyoung Son <taeyoungson10@gmail.com>
2022-11-17 18:23:26 +0900

    feat: RxBaseTableCell 정의 - RxDatasourceCell 프로토콜 정의

---
+++ Features/Chat/Sources/Presentation/Base/RxBaseTableCell.swift
@@ -0,0 +1,39 @@
+//
+//  RxBaseTableCell.swift
+//  Chat
+//
+//  Created by Taeyoung Son on 2022/11/17.
+//
+
+import UIKit
+
+import RxSwift
+import COCommonUI
+
+protocol RxDatasourceCell: RxBindable {
+    associatedtype SectionItem
+    func configure(with item: SectionItem)
+}
+
+class RxBaseTableCell<SectionItem>: UITableViewCell, FlexLayoutType, RxDatasourceCell, RxBindable {
+    
+    var disposeBag: DisposeBag = .init()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        self.setup()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    func setupContainer() { }
+    func layout() { }
+    func setAttrs() { }
+    func bind() { }
+    func clearBag() {
+        self.disposeBag = .init()
+    }
+    func configure(with item: SectionItem) { }
+}
