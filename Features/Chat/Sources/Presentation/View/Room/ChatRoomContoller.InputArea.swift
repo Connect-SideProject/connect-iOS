37d65b7453f25eb67edf98539779dda347bfad82
Taeyoung Son <taeyoungson10@gmail.com>
2022-11-22 22:14:51 +0900

    feat: ChatRoomContoller.InputArea 생성

---
+++ Features/Chat/Sources/Presentation/View/Room/ChatRoomContoller.InputArea.swift
@@ -0,0 +1,16 @@
+//
+//  ChatRoomContoller.InputArea.swift
+//  Chat
+//
+//  Created by Taeyoung Son on 2022/11/22.
+//
+
+import UIKit
+
+import COCommonUI
+
+extension ChatRoomController {
+    final class InputArea: FlexLayoutView {
+        
+    }
+}
