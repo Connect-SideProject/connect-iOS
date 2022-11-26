68beaadc5bf2967ed41494b606eb21e0d50a91eb
Taeyoung Son <taeyoungson10@gmail.com>
2022-11-26 16:47:07 +0900

    feat: InputArea 에 send 버튼 레이아웃 정의

--- Features/Chat/Sources/Presentation/View/Room/ChatRoomContoller.InputArea.swift
+++ Features/Chat/Sources/Presentation/View/Room/ChatRoomContoller.InputArea.swift
@@ -14,7 +14,7 @@ import COCommonUI
 extension ChatRoomController {
     final class InputArea: FlexLayoutView {
         private let textField = UITextField()
-        private let sendBtn = RoundRutton()
+        private let sendBtn = RoundRutton(cornerRadius: 12)
         
         override func setupContainer() {
             super.setupContainer()
@@ -27,13 +27,27 @@ extension ChatRoomController {
                         .grow(1)
                         .marginHorizontal(20)
                         .height(44)
+                        .alignItems(.end)
+                        .define {
+                            $0.addItem(self.sendBtn)
+                                .width(48).height(44)
+                        }
                 }
-                
         }
         
         override func layout() {
             self.rootContainer.pin.all()
             self.rootContainer.flex.layout()
+        }
+        
+        override func setAttrs() {
+            super.setAttrs()
+            self.textField.layer.cornerRadius = 12
+            self.textField.layer.borderWidth = 1
+            self.textField.layer.borderColor = UIColor.hexC6C6C6.cgColor
+            self.sendBtn.layer.backgroundColor = UIColor.hex05A647.cgColor
+            self.sendBtn.layer.borderWidth = .zero
+            self.sendBtn.setImage(.init(named: "ic_chat_send_msg"), for: .normal)
         }
     }
 }
