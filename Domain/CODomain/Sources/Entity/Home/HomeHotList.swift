f190f0a70b002aacec67592c8b5efc4a3173ddb0
Do-Hyun-Kim <dohyun8032@gmail.com>
2022-11-28 21:27:19 +0900

    feat: HomeHotList Domain fix  - HomeHotList Identifier Protocol 채택 or filed 값 추가

--- Domain/CODomain/Sources/Entity/Home/HomeHotList.swift
+++ Domain/CODomain/Sources/Entity/Home/HomeHotList.swift
@@ -7,24 +7,40 @@
 
 import Foundation
 
-public struct HomeHotList: Codable {
-    
-    public var releaseStudyType: String
+public struct HomeHotList: Codable, Identifiable {
+    public var id: String
+    public var releaseisEnd: Bool
     public var releaseTitle: String
     public var releaseBookMark: Int
+    public var releaseMyBookMark: Bool
     public var releaseStudyInfo: String
-    public var releaseRecruitPart: [String]
-    public var releaseCreateAt: String
-    public var releaseisEnd: Bool
+    public var releaseRecruitPart: [HomePartsList]
+    public var releaseUserId: String
+    
     
     
     enum CodingKeys: String, CodingKey {
-        case releaseStudyType = "studyType"
+        case id
+        case releaseisEnd = "isEnd"
         case releaseTitle = "studyTitle"
         case releaseBookMark = "bookmark"
+        case releaseMyBookMark = "myBookmark"
         case releaseStudyInfo = "studyInfo"
-        case releaseRecruitPart = "recruitmentParts"
-        case releaseCreateAt = "creatDt"
-        case releaseisEnd = "isEnd"
+        case releaseRecruitPart = "parts"
+        case releaseUserId = "userId"
     }
+}
+
+
+public struct HomePartsList: Codable {
+    public var role: String
+    public var partsCount: Int
+    
+    
+    enum CodingKeys: String, CodingKey {
+        case role
+        case partsCount = "count"
+    }
+    
+    
 }
