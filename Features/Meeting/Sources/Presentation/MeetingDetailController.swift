9c0338f2023920cd448355f355b1813733db7828
Taeyoung Son <taeyoungson10@gmail.com>
2022-12-04 18:15:23 +0900

    feat: MeetingDetailController 파일 생성

---
+++ Features/Meeting/Sources/Presentation/MeetingDetailController.swift
@@ -0,0 +1,12 @@
+//
+//  MeetingDetailController.swift
+//  Meeting
+//
+//  Created by Taeyoung Son on 2022/12/04.
+//
+
+import UIKit
+
+public final class ChatListController: UIViewController {
+    
+}
