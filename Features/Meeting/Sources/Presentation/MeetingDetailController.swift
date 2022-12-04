dd912fda5c9ac8bb98ec4e277293ceb0a3398b22
Taeyoung Son <taeyoungson10@gmail.com>
2022-12-04 23:59:20 +0900

    feat: 모임 상세화면 ReactorBaseC 상속받도록 수정

--- Features/Meeting/Sources/Presentation/MeetingDetailController.swift
+++ Features/Meeting/Sources/Presentation/MeetingDetailController.swift
@@ -7,6 +7,25 @@
 
 import UIKit
 
-public final class ChatListController: UIViewController {
-    
+import RxSwift
+import ReactorKit
+import COCommonUI
+
+public final class MeetingDetailController: ReactorBaseController<MeetingDetailController.Reactor> {
+    private let titleView = TitleView()
+    private let topArea = TopArea()
+}
+
+extension MeetingDetailController {
+    final public class Reactor: ReactorKit.Reactor {
+        public var initialState = State()
+        public enum Action { }
+        public struct State { }
+    }
+}
+
+private extension MeetingDetailController {
+    final class TopArea {
+        
+    }
 }
