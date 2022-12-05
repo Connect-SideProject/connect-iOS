37a6a14ac566f2a4cfe716170d2a7df7231b2c6a
Taeyoung Son <taeyoungson10@gmail.com>
2022-12-06 00:02:28 +0900

    feat: 모임 상세 서브뷰들 추가

--- Features/Meeting/Sources/Presentation/MeetingDetailController.swift
+++ Features/Meeting/Sources/Presentation/MeetingDetailController.swift
@@ -10,11 +10,44 @@ import UIKit
 import RxSwift
 import ReactorKit
 import COCommonUI
+import COExtensions
 
 public final class MeetingDetailController: ReactorBaseController<MeetingDetailController.Reactor> {
     private let titleView = TitleView()
     private let topArea = TopArea()
     private let tableView = UITableView()
+    
+    public override func setupContainer() {
+        super.setupContainer()
+        
+        self.rootContainer.flex.define { [weak self] in
+            guard let self = self else { return }
+            $0.addItem(self.titleView)
+                .height(50)
+            $0.addItem(self.topArea)
+                .height(128)
+            $0.addItem(self.tableView)
+                .grow(1)
+        }
+    }
+    
+    public override func layout() {
+        self.rootContainer.pin.all()
+        let safe = UIApplication.keyWindow?.safeAreaInsets
+        self.rootContainer.flex.margin(safe ?? self.view.safeAreaInsets).layout()
+    }
+    
+    public override func setAttrs() {
+        super.setAttrs()
+        self.setTitleView()
+    }
+    
+    private func setTitleView() {
+        self.titleView.set(title: "프로젝트 정보")
+            .setLeftBtn(type: .back)
+            .setRightInnerBtn(type: .star)
+            .setRightOuterBtn(type: .share)
+    }
 }
 
 extension MeetingDetailController {
@@ -27,7 +60,11 @@ extension MeetingDetailController {
 
 extension MeetingDetailController {
     final class TopArea: FlexLayoutView {
-        
+        private let titleLabel = UILabel()
+        private let avatarView = ProfileImageView()
+        private let profileLabel = UILabel()
+        private let tabItems = [UIButton(), UIButton(), UIButton()]
+        private let tabUnderLineView = UIView()
     }
 }
 
@@ -37,6 +74,7 @@ extension MeetingDetailController {
     }
     
     final class TextTableCell: RxBaseTableCell<Any> {
-        
+        private let titleLabel = UILabel()
+        private let descLabel = UILabel()
     }
 }
