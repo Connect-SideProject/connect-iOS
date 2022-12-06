32e3be9802bb849d69d203c73bf0dd9305f1a34b
Taeyoung Son <taeyoungson10@gmail.com>
2022-12-07 00:03:12 +0900

    feat: MeetingDetailController.TopArea 레이아웃 정의

--- Features/Meeting/Sources/Presentation/MeetingDetailController.swift
+++ Features/Meeting/Sources/Presentation/MeetingDetailController.swift
@@ -65,6 +65,55 @@ extension MeetingDetailController {
         private let profileLabel = UILabel()
         private let tabItems = [UIButton(), UIButton(), UIButton()]
         private let tabUnderLineView = UIView()
+        
+        override func setupContainer() {
+            self.flex.define { [weak self] in
+                guard let self = self else { return }
+                $0.addItem(self.titleLabel)
+                    .marginTop(16)
+                    .height(22)
+                    .marginHorizontal(20)
+                
+                $0.addItem().direction(.row)
+                    .height(24)
+                    .marginTop(8)
+                    .marginHorizontal(20)
+                    .define {
+                        $0.addItem(self.avatarView)
+                            .width(24).height(24)
+                        
+                        $0.addItem(self.profileLabel)
+                            .marginLeft(8)
+                    }
+                
+                $0.addItem().direction(.row)
+                    .marginTop(30)
+                    .alignItems(.baseline)
+                    .justifyContent(.spaceEvenly)
+                    .define { flex in
+                        self.tabItems.enumerated().forEach { index, item in
+                            flex.addItem(item)
+                                .height(19)
+                        }
+                    }
+                
+                $0.addItem(self.tabUnderLineView)
+                    .marginTop(8)
+                    .width(41).height(1)
+            }
+        }
+        
+        override func setAttrs() {
+            super.setAttrs()
+            self.setTabItems()
+        }
+        
+        private func setTabItems() {
+            let titles = ["모집정보", "모임상세", "모임위치"]
+            self.tabItems.enumerated().forEach { idx, btn in
+                btn.setTitle(titles[idx], for: .normal)
+            }
+        }
     }
 }
 
