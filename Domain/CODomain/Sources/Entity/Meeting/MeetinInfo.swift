537d1ee98808b574e0e03a78779731e9705dd6eb
Taeyoung Son <taeyoungson10@gmail.com>
2022-12-07 22:21:01 +0900

    feat: 모임 상세 모델 추가

---
+++ Domain/CODomain/Sources/Entity/Meeting/MeetinInfo.swift
@@ -0,0 +1,52 @@
+//
+//  MeetingInfo.swift
+//  CODomain
+//
+//  Created by Taeyoung Son on 2022/12/07.
+//
+
+import Foundation
+
+// MARK: - MeetingInfo
+struct MeetingInfo: Codable {
+    let id: Int
+    let userID, nickName: String
+    let profileURL: String
+    let roles: [MeeingInfoRole]
+    let endDate, studyType, meetingType: String
+    let categories: [Category]
+    let parts: [String]
+    let studyInfo, aspiration: String
+    let bookmark: Int
+    let place: Place
+    let end: Bool
+
+    enum CodingKeys: String, CodingKey {
+        case id
+        case userID = "userId"
+        case nickName
+        case profileURL = "profileUrl"
+        case roles, endDate, studyType, meetingType, categories, parts, studyInfo, aspiration, bookmark, place, end
+    }
+}
+
+// MARK: - Category
+struct Category: Codable {
+    let category: String
+}
+
+// MARK: - Place
+struct Place: Codable {
+    let name: String
+    let location: Location
+}
+
+// MARK: - Location
+struct Location: Codable {
+    let x, y: Double
+}
+
+// MARK: - MeeingInfoRole
+struct MeeingInfoRole: Codable {
+    let role: RoleType
+}
