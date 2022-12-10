10e6651463f64efa869033f128dd37185139e369
Taeyoung Son <taeyoungson10@gmail.com>
2022-12-11 00:21:58 +0900

    feat: CardTableCell 레이아웃 정의

--- Features/Meeting/Sources/Presentation/MeetingDetail/MeetingDetailController.TableCell.swift
+++ Features/Meeting/Sources/Presentation/MeetingDetail/MeetingDetailController.TableCell.swift
@@ -7,13 +7,49 @@
 
 import UIKit
 
+import FlexLayout
 import COCommonUI
 import CODomain
 import RxDataSources
 
 extension MeetingDetailController {
     final class CardTableCell: RxBaseTableCell<Any> {
+        private let titleLabel = UILabel()
+        private let cardContainer = UIView()
+        private let cardTitleArea = VStackView()
+        private let cardValueArea = VStackView()
         
+        override func setupContainer() {
+            self.contentView.flex.define {
+                $0.addItem(self.titleLabel)
+                    .marginTop(22)
+                    .marginLeft(20)
+                
+                $0.addItem(self.cardContainer)
+                    .marginTop(15)
+                    .marginHorizontal(20)
+                    .marginBottom(35)
+                    .direction(.row)
+                    .define {
+                        $0.addItem(self.cardTitleArea)
+                            .minWidth(17)
+                            .marginLeft(25)
+                        
+                        $0.addItem(self.cardValueArea)
+                            .marginLeft(25)
+                    }
+            }
+        }
+        
+        public override func layout() {
+            self.contentView.flex.layout(mode: .adjustHeight)
+        }
+        
+        override func sizeThatFits(_ size: CGSize) -> CGSize {
+            self.contentView.pin.width(size.width)
+            self.layout()
+            return self.contentView.frame.size
+        }
     }
     
     final class TextTableCell: RxBaseTableCell<Any> {
