7c68c2f2952f997677a3caa0f325024c63e574d6
Taeyoung Son <taeyoungson10@gmail.com>
2022-12-12 00:03:18 +0900

    refactor: MeetingInfo 접근 제한자 변경

--- Domain/CODomain/Sources/Entity/Meeting/MeetinInfo.swift
+++ Domain/CODomain/Sources/Entity/Meeting/MeetinInfo.swift
@@ -9,17 +9,17 @@ import Foundation
 
 // MARK: - MeetingInfo
 public struct MeetingInfo: Codable {
-    let id: Int
-    let userID, nickName: String
-    let profileURL: String
-    let roles: [MeeingInfoRole]
-    let endDate, studyType, meetingType: String
-    let categories: [Category]
-    let parts: [Part]
-    let studyInfo, aspiration: String
-    let bookmark: Int
-    let place: Place
-    let end: Bool
+    public var id: Int
+    public var userID, nickName: String
+    public var profileURL: String
+    public var roles: [MeeingInfoRole]
+    public var endDate, studyType, meetingType: String
+    public var categories: [Category]
+    public var parts: [Part]
+    public var studyInfo, aspiration: String
+    public var bookmark: Int
+    public var place: Place
+    public var end: Bool
 
     enum CodingKeys: String, CodingKey {
         case id
@@ -31,28 +31,28 @@ public struct MeetingInfo: Codable {
 }
 
 // MARK: - Category
-struct Category: Codable {
+public struct Category: Codable {
     let category: String
 }
 
 // MARK: - Place
-struct Place: Codable {
+public struct Place: Codable {
     let name: String
     let location: Location
 }
 
 // MARK: - Location
-struct Location: Codable {
+public struct Location: Codable {
     let x, y: Double
 }
 
 // MARK: - MeeingInfoRole
-struct MeeingInfoRole: Codable {
+public struct MeeingInfoRole: Codable {
     let role: RoleType
 }
 
 // MARK: - Part
-struct Part: Codable {
+public struct Part: Codable {
     let count: Int
     let role: RoleType
 }
