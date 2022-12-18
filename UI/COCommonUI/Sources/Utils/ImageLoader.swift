2ab3e15e3917d9de1d18e0e7d3bb8e06dcc1348b
sean <butterfree@kakao.com>
2022-12-18 22:15:55 +0900

    refactor: ImageLoader 강제 언래핑 제거 진행

--- UI/COCommonUI/Sources/Utils/ImageLoader.swift
+++ UI/COCommonUI/Sources/Utils/ImageLoader.swift
@@ -10,7 +10,7 @@ import Foundation
 import UIKit
 
 public protocol ImageLoadable {
-  func fetch(url: URL) async throws -> UIImage
+  func fetch(url: URL) async throws -> UIImage?
 }
 
 public actor ImageLoader: ImageLoadable {
@@ -21,20 +21,19 @@ public actor ImageLoader: ImageLoadable {
     self.cache = cache
   }
   
-  public func fetch(url: URL) async throws -> UIImage {
+  public func fetch(url: URL) async throws -> UIImage? {
     
     let request = URLRequest(url: url)
     
     if let cachedResponse = cache.cachedResponse(for: request) {
-      let image = UIImage(data: cachedResponse.data)!
+      let image = UIImage(data: cachedResponse.data)
       return image
     }
     
-    let task: Task<UIImage, Error> = Task {
+    let task: Task<UIImage?, Error> = Task {
       let (imageData, response) = try await URLSession.shared.data(for: request)
-      let image = UIImage(data: imageData)!
       cache.storeCachedResponse(.init(response: response, data: imageData), for: request)
-      return image
+      return UIImage(data: imageData)
     }
     
     let image = try await task.value
